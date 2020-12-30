7c8fd5ae92be34fe3fb48acac0d4135ed9bc8a81
ichino <chinoknct@gmail.com>
2020-12-30 15:57:41 +0900

    [add]第4章まで追加

---
+++ 04/fill/Fill.asm
@@ -0,0 +1,87 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed.
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// screen size
+@30
+D=A
+@SCREEN_SIZE
+M=D
+
+(LOOP)
+
+  // keyboard input judge
+  @KBD
+  D=M
+
+  // number from the beginning
+  @COUNT
+  M=0
+
+  @FILL_LOOP
+  D;JNE
+
+  // clear loop
+  (CLEAR_LOOP)
+    @KBD
+    D=M
+
+    @LOOP
+    D;JNE
+
+    @COUNT
+    D=M
+    @SCREEN_SIZE
+    D=D-M
+    @LOOP
+    D;JGT
+
+    @COUNT
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+    @COUNT
+    M=M+1
+    @CLEAR_LOOP
+    0;JMP
+
+  // draw loop
+  (FILL_LOOP)
+    @KBD
+    D=M
+
+    @LOOP
+    D;JEQ
+
+    // confirmation of finish of painting
+    @COUNT
+    D=M
+    @SCREEN_SIZE
+    D=D-M
+    @LOOP
+    D;JGT
+
+    @COUNT
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+    @COUNT
+    M=M+1
+    @FILL_LOOP
+    0;JMP
+
+  @LOOP
+  0;JMP
+
+(END)
